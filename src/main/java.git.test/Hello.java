853b8e7fb3fcd2831b14e1919f7c9279ef92b0c5
gaydukovav284@gmail.com <gaydukovav284@gmail.com>
2021-09-09 09:13:23 +0300

    nnn

--- src/main/java.git.test/Hello.java
+++ src/main/java.git.test/Hello.java
@@ -3,6 +3,7 @@ package main.java.git.test;
 import org.w3c.dom.ls.LSOutput;
 
 public class Hello {
+
     public void hello(){
         System.out.println("hello");
     }
--- src/main/java.git.test/MainClass.java
+++ src/main/java.git.test/MainClass.java
@@ -3,5 +3,7 @@ package main.java.git.test;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello word!!!");
+        Hello hello = new Hello();
+        hello.hello();
     }
 }
